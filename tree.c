b4881c63a483409f0c1b966b35a893ccd401337e
Miguel Rubi <rsnakdmx@hotmail.com>
2018-12-18 12:50:12 -0600

    Se reidento el codigo

--- tree.c
+++ tree.c
@@ -6,67 +6,77 @@
 
 void listarDir(const char*, int64_t, int64_t*, int64_t*); //Prototipo
 
-int32_t main(const int32_t argc, const char *argv[]){
+int32_t main(const int32_t argc, const char *argv[])
+{
   //Se valida la cantidad de argumentos pasados
-  if (argc == 2) {
-    int64_t dirs= 0, archs= 0; //Variables donde se cuentan los elementos
-    printf("\n\n[%s]\n",argv[1]);
-    listarDir(argv[1], 1, &dirs, &archs); //Se pasan por referencia
-    printf("\nDirectorios: %jd, Archivos: %jd\n",dirs,archs);
-    return EXIT_SUCCESS;
+    if (argc == 2) {
+        int64_t dirs= 0, archs= 0; //Variables donde se cuentan los elementos
+        printf("\n\n[%s]\n",argv[1]);
+        listarDir(argv[1], 1, &dirs, &archs); //Se pasan por referencia
+        printf("\nDirectorios: %jd, Archivos: %jd\n",dirs,archs);
+        return EXIT_SUCCESS;
+    }
 
-  }else{
-    printf("Error al ingresar los parametros\n");
-    return EXIT_FAILURE;
-  }
+    else
+    {
+        printf("Error al ingresar los parametros\n");
+        return EXIT_FAILURE;
+    }
 }
 
-void listarDir(const char *rutaParcial,int64_t nivel,int64_t *d,int64_t *a){
-  char *ruta= (char*)calloc(1024,sizeof(char)); //Memoria para la ruta
-  DIR *carpeta; //Flujo
-  struct dirent *contenido; //Contenido de la carpeta
+void listarDir(const char *rutaParcial,int64_t nivel,int64_t *d,int64_t *a)
+{
+    char *ruta= (char*)calloc(1024,sizeof(char)); //Memoria para la ruta
+    DIR *carpeta; //Flujo
+    struct dirent *contenido; //Contenido de la carpeta
 
-  //Se comprueba que hay espacio para la ruta
-  if (!ruta) {
-    perror("Error: ");
-    return;
-  }
-
-  //Se comprueba que sean directorios validos
-  if (!(carpeta = opendir(rutaParcial))) {
-    perror("Error: ");
-    return;
-  }
-
-  if (!(contenido = readdir(carpeta))) {
-    perror("Error: ");
-    return;
-  }
-
-  do {
-    //Se verifica si la entrada es un archivo o un directorio
-    if (contenido->d_type == DT_DIR) {
-      //Se escribe la ruta siguiente a usarse
-      snprintf(ruta,1023,"%s/%s",rutaParcial,contenido->d_name);
-
-      //No se imprimen . ni ..
-      if (strcmp(contenido->d_name, ".") == 0 ||
-        strcmp(contenido->d_name, "..") == 0)
-          continue;
-
-      //Se usa %*s para hacer las sangrias correspondientes al nivel
-      printf("%*s[%s]\n", (int32_t)nivel*4, "", contenido->d_name);
-      (*d)++; //Se incrementa el contenido
-      listarDir(ruta, nivel+1,d,a); //Se muestra la carpeta siguiente
-      //Se sigue enviando las referencias
-
-    } else { //Caso directorio
-      printf("%*s- %s\n", (int32_t)nivel*4, "", contenido->d_name);
-      (*a)++;
+    //Se comprueba que hay espacio para la ruta
+    if (!ruta)
+    {
+        perror("Error: ");
+        return;
     }
-  } while ((contenido= readdir(carpeta))!= NULL);
 
-  //Se cierra el flujo
-  closedir(carpeta);
-  free(ruta); //Se libera la memoria
+    //Se comprueba que sean directorios validos
+    if (!(carpeta = opendir(rutaParcial)))
+    {
+        perror("Error: ");
+        return;
+    }
+
+    if (!(contenido = readdir(carpeta))) 
+    {
+        perror("Error: ");
+        return;
+    }
+
+    do 
+    {
+        //Se verifica si la entrada es un archivo o un directorio
+        if (contenido->d_type == DT_DIR)
+        {
+            //Se escribe la ruta siguiente a usarse
+            snprintf(ruta,1023,"%s/%s",rutaParcial,contenido->d_name);
+
+            //No se imprimen . ni ..
+            if (strcmp(contenido->d_name, ".") == 0 || strcmp(contenido->d_name, "..") == 0)
+                continue;
+
+            //Se usa %*s para hacer las sangrias correspondientes al nivel
+            printf("%*s[%s]\n", (int32_t)nivel*4, "", contenido->d_name);
+            (*d)++; //Se incrementa el contenido
+            listarDir(ruta, nivel+1,d,a); //Se muestra la carpeta siguiente
+            //Se sigue enviando las referencias
+        }
+
+        else 
+        { //Caso directorio
+            printf("%*s- %s\n", (int32_t)nivel*4, "", contenido->d_name);
+            (*a)++;
+        }
+    } while ((contenido= readdir(carpeta))!= NULL);
+
+    //Se cierra el flujo
+    closedir(carpeta);
+    free(ruta); //Se libera la memoria
 }
